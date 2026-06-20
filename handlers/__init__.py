befaae73d3f0ff1390fbfb1b6ae8603c69ea2c69
Ramzio <Sinistraz89@gmail.com>
2026-06-20 20:15:04 +0300

    feat: recommend command added, README added style: recommend.jpg, translate.jpg added

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,11 +1,12 @@
 from aiogram import Router
-# Замінюємо відносні імпорту на абсолютні від кореня проекту
-from handlers import start, random, chat_gpt, talk, quiz, translate
+from handlers import start, random, chat_gpt, talk, quiz, translate, recommend
 from util import default_callback_handler
 
 main_router = Router()
 
-# Створюємо окремий роутер суворо для дефолтних речей
+# Створюємо окремий роутер суворо для дефолтних речей, так як чомусь дефолтний роут
+# перехоплює все підряд
+
 fallback_router = Router()
 fallback_router.callback_query.register(default_callback_handler)
 
@@ -16,5 +17,5 @@ main_router.include_router(chat_gpt.router)
 main_router.include_router(talk.router)
 main_router.include_router(quiz.router)
 main_router.include_router(translate.router)
-# Суворо НАЙОСТАННІМ підключаємо дефолтний роутер
+main_router.include_router(recommend.router)
 main_router.include_router(fallback_router)
